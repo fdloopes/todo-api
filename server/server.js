1d273b230f63b2bfc3673f375e6fb8bbb6e36bf2
fdloopes <fdloopes@gmail.com>
2018-11-16 20:33:40 -0200

    rota post todo

--- server/server.js
+++ server/server.js
@@ -10,8 +10,17 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
-  console.log(res.body);
+  var todo = new Todo({
+    text:req.body.text
+  });
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(error)=>{
+    res.status(400).send(error);
+  });
 });
+
 
 app.listen(3000, ()=>{
   console.log('Started on port 3000');
