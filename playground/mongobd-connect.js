d99ad6f0d87f5bfd5827de3675739d28aa8ecb4a
fdloopes <fdloopes@gmail.com>
2018-11-16 14:15:42 -0200

    Init Commit

---
+++ playground/mongobd-connect.js
@@ -0,0 +1,50 @@
+
+//const {MongoClient, ObjectID} = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+//var obj = new ObjectID();
+//console.log(obj);
+
+var user = {name:'João',age:21};
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true },(error,client)=>{
+  if(error){
+    return console.log('Unable to connect to MongoDB server',error);
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  db.collection('Users').insertOne(
+    user,(error,result)=>{
+      if(error){
+        return console.log('Unable to insert todo',error);
+      }
+      console.log(JSON.stringify(result.ops,undefined,2));
+    }
+  );
+
+  /*db.collection('Todos').insertOne({
+    text: 'Something to do',
+    completed: false
+  },(error,result)=>{
+    if(error){
+    return console.log('Unable to insert todo',error);
+    }
+    console.log(JSON.stringify(result.ops,undefined,2));
+  });*/
+
+/*  db.collection('Users').insertOne({
+    name: 'Felipe',
+    age: 27,
+    location: 'Pelotas'
+  },(error,result)=>{
+    if(error){
+      return console.log('Unable to insert to do',error);
+    }
+    console.log(JSON.stringify(result.ops,undefined,2));
+    console.log(result.ops[0]._id.getTimestamp());
+  });*/
+
+  //db.collection('User')inse
+
+  client.close();
+});
