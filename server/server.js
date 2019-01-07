12c9857b433105791c36620cafbc46b50484a234
Felipe Lopes <fdloopes@gmail.com>
2019-01-07 13:17:06 -0200

    add cases test /todos/:id

--- server/server.js
+++ server/server.js
@@ -22,6 +22,20 @@ app.post('/todos',(req,res)=>{
   });
 });
 
+app.post('/user',(req,res)=>{
+  var user = new User({
+    email:req.body.email,
+    name:req.body.name,
+    senha:req.body.senha
+  });
+
+  user.save().then((doc)=>{
+    res.send(doc);
+  },(error)=>{
+    res.status(400).send(error);
+  });
+});
+
 app.get('/todos',(req,res)=>{
   Todo.find().then((todos)=>{
     res.send({todos});
@@ -45,6 +59,29 @@ app.get('/todos/:id',(req,res) =>{
   });
 });
 
+app.get('/user',(req,res)=>{
+  User.find().then((users)=>{
+    res.send({users});
+  },(error)=>{
+    res.status(400).send(error);
+  });
+});
+
+app.get('/user/:id',(req,res) =>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  User.findById(id).then((user)=>{
+    if(!user){
+      return res.status(404).send();
+    }
+    res.status(200).send({user});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+});
+
 app.delete('/todos',(req,res)=>{
   Todo.deleteOne({
     text:req.body.text
@@ -57,6 +94,20 @@ app.delete('/todos',(req,res)=>{
   },(error)=>{
     res.status(400).send(error);
   });
+});
+
+app.delete('/user',(req,res)=>{
+    User.deleteOne({
+      email:req.body.email
+    }).then((result)=>{
+      if(result.n>0){
+        res.send('Usuário deletado');
+      }else{
+        res.send('Email não encontrado');
+      }
+    },(error)=>{
+      res.status(400).send(error);
+    });
 });
 
 app.listen(3000, ()=>{
